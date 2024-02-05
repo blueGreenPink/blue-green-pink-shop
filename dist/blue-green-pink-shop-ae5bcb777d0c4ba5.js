52feb61817bf55b9a7b85d851610d5a73bc454fc
devUserContact <developer.user.contact@protonmail.com>
2024-02-05 13:54:18 -0800

    added item to shop

--- dist/blue-green-pink-shop-ae5bcb777d0c4ba5.js
+++ dist/blue-green-pink-shop-ae5bcb777d0c4ba5.js
@@ -836,7 +836,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('blue-green-pink-shop-a1a4745ce7fc6b0a_bg.wasm', import.meta.url);
+        input = new URL('blue-green-pink-shop-ae5bcb777d0c4ba5_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
